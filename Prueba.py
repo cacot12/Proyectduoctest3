68bae79f30e332b473469965d6611eef4455a9ec
cacot12 <137566960+cacot12@users.noreply.github.com>
2023-06-27 05:36:17 -0400

    Update Prueba.py

--- Prueba.py
+++ Prueba.py
@@ -2,16 +2,21 @@ import random
 salir=True
 opcion=0
 Autos={}
+validar=False
 while salir:
     print("Opción 1:Grabar\nOpción 2:Buscar \nOpción 3:Imprimir certificados\nOpción 4:Salir")
-    opcion=int(input("Escoge una opción: "))
-    while opcion >4 or opcion <1 :
-        print("Digite una Opción valida\nOpción 1:Grabar\nOpción 2:Buscar \nOpción 3:Imprimir certificados\nOpción 4:Salir")
-        opcion=int(input("Escoge una opción valida: "))
+    opcion=input("Escoge una opción: ")
+    while True:
+        if len(opcion)==1:
+            if opcion in "1234":
+                opcion=int(opcion)
+                break
+        opcion=input("Escoge una opción valida: ")
     caracteristicas=[]
     multas=[]
     a=""
     if opcion==1:
+        validar=True
         patente=input("ingrese una patente valida (entre 2 y 15): ")
         while True:
             if len(patente)>1 and len(patente)<16:
@@ -19,23 +24,35 @@ while salir:
             patente=input("ingrese una patente valida (entre 2 y 15): ")
         tipo=input("Ingrese el tipo del vehiculo: ")
         marca=input("Ingrese la marca del vehiculo: ")
-        precio=int(input("Ingrese precio del vehículo: "))
+        precio=(input("Ingrese precio del vehículo: "))
         while True:
-            if precio >5000000:
-                break
-            precio=int(input("Ingrese precio del vehículo: "))
+            if precio=="":
+                precio=(input("Ingrese precio valido del vehículo (Mayor a 5 millones): "))
+            else:
+                c=0
+                for t in precio:
+                    if t in "0123456789":
+                        c+=1
+                    else:
+                        precio=(input("Ingrese precio valido del vehículo (Mayor a 5 millones): "))
+                        break
+                    if c==len(precio):
+                        precio=int(precio)
+                if isinstance(precio,int):
+                    if precio >5000000:
+                        break
+                    precio=(input("Ingrese precio valido del vehículo (Mayor a 5 millones): "))
         print("¿Tiene multas? \n1.-Si\n2._No")
         while True:
-            a=int(input())
-            if a==1:
-                multas.append((random.randrange(1500,3000)))
-                multas.append(input("Ingrese fecha de la multa"))
-            elif a==2:
+            a=input()
+            if a=="1":
+                multas.append((random.randrange(1500,3000),input("Ingrese fecha de la multa: ")))
+            elif a=="2":
                 if multas==[]:
                     multas.append("No posee multas")
                 break
             else:
-                print("Marque una opción Valida: ")
+                print("Marque una opción Valida ( '1' o '2'): ")
             print("¿Tiene más multas? \n1.-Si\n2._No")
         fecha_registro=input("Ingrese fecha de registro del vehículo: ")
         dueño=input("ingrese el dueño del vehículo: ")
@@ -46,19 +63,25 @@ while salir:
         caracteristicas.append(fecha_registro)
         caracteristicas.append(dueño)
         Autos[patente]=caracteristicas
-    if opcion==2:
-        busqueda=input("ingrese una patente valida (entre 2 y 15): ")
+    if opcion==2 and validar:
+        busqueda=input("ingrese una patente previamente registrada: ")
         while True:
-            if len(busqueda)>1 and len(busqueda)<16:
+            if busqueda in Autos:
                 break
-            busqueda=input("ingrese una patente valida (entre 2 y 15): ")
+            busqueda=input("ingrese una patente previamente registrada: ")
         print(Autos[busqueda])
-    if opcion==3:
-        busqueda=input("ingrese una patente valida (entre 2 y 15): ")
+    if opcion==3 and validar:
+        busqueda=input("ingrese una patente previamente registrada: ")
         while True:
-            if len(busqueda)>1 and len(busqueda)<16:
+            if busqueda in Autos:
                 break
-            busqueda=input("ingrese una patente valida (entre 2 y 15): ")
-        print("Certificados de Emisión de contaminantes\nPatente ",busqueda,"\nDueño: ",Autos[busqueda][5],"\nFecha del registro: ", Autos[busqueda][4],"\nMultas: ",Autos[busqueda][3])
+            busqueda=input("ingrese una patente previamente registrada: ")
+        print("\n\nCertificados de Emisión de contaminantes\n\n\nPatente: ",busqueda,"\nDueño: ",Autos[busqueda][5],"\nFecha del registro: ", Autos[busqueda][4],"\nMultas: ")
+        for q,w in Autos[busqueda][3]:
+            print("$",q,"El día: ",w)
     if opcion==4:
-        salir=False
+        break
+    if not validar:
+        print("**********************    **********************\n**********************    **********************\n   Debe grabar primero un Registro de vehículo\n**********************    **********************\n**********************    **********************\n")
+
+
